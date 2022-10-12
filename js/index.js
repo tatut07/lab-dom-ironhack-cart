0f14532f47626746cb7d7b9cd43bda37fc39b364
Tatiana Toledo <tatiana@Tatianas-MacBook-Pro.local>
2022-10-12 17:29:22 +0200

    iteration3

--- js/index.js
+++ js/index.js
@@ -15,24 +15,14 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let totalPriceSum = 0;
 
-  // ITERATION 2
-  let totalPrice = 0;
-  const allProducts = document
-    .querySelectorAll('.product')
-    .forEach((product) => {
-      let singlePrice = updateSubtotal(product);
-      totalPrice += singlePrice;
-      return totalPrice;
-    });
-
-  // ITERATION 3
-  //... your code goes here
+  document.querySelectorAll('.product').forEach((product) => {
+    const singlePrice = updateSubtotal(product);
+    totalPriceSum += singlePrice;
+  });
+  const finalPrice = document.querySelector('#total-value span');
+  finalPrice.innerText = totalPriceSum;
 }
 
 // ITERATION 4
